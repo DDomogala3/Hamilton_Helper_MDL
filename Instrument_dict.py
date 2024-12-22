aeac8470564b06ca1d40a9ab1ce71c187fb398eb
DDomogala3 <ddd38@nau.edu>
2024-12-21 19:17:09 -0600

    Dictionary file

---
+++ Instrument_dict.py
@@ -0,0 +1,7 @@
+#Dictionary of Hamilton Instruments
+
+Hamilton_Dict = {"Star Presto 1": "D178", "Star Presto 2": "1209", "Star Presto 3": "1210",
+                 "Easy Blood2": "1196", "NGS POST1": "G105", "NGS PRE1": "G106",
+                 "NGS PRE2": "G109", "NGS-POST2": "G103", "NGS-POST3": "D167"}
+for key in Hamilton_Dict:
+    print(key)
